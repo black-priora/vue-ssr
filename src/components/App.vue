a07a4a688f2d917a099318c5977e048516446bfe
Rustem Khakimzyanov <rustem.khakimzyanov@gmail.com>
2018-09-20 13:34:56 +0300

    VUE ssr

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <div v-for='l in list' v-bind:key='l.id'>
-      <div @click='click'>{{ l.name }}</div>
-      <div>{{ l.notice }}</div>
-    </div>
-    <div @click='click'>@@</div>
+    app
     <router-view></router-view>
     <router-link :to="{ name: 'about' }">About</router-link>
     <router-link :to="{ name: 'contact' }">Contact</router-link>
@@ -12,45 +8,5 @@
 </template>
 <script>
   export default {
-    // props: ['list'],
-    //
-    data: () => ({
-      list: []
-    }),
-
-    methods: {
-      click() {
-        alert(1)
-      }
-    },
-
-    created() {
-      try {
-        var XMLHttpRequest = PHP.XMLHttpRequest;
-        var ajax = new XMLHttpRequest();
-
-        ajax.open('GET', 'http://abn-corp.it-effects.ru/api/news/', false);
-
-
-        ajax.onreadystatechange = function () {
-          print(1)
-        }
-
-        ajax.send({})
-
-        // this.data = []
-
-        if (ajax.status === 200) {
-          this.list = (JSON.parse(ajax.responseText)).data.list;
-        }
-      } catch (e) {}
-    },
-
-    // mounted () {
-    //     this.$http.get('http://abn-corp.it-effects.ru/api/news/')
-    //       .then(res => {
-    //         this.list = res.data.data.list
-    //       })
-    // }
   }
 </script>
